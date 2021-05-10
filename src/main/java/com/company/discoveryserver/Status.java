186e56e7ac636bb829f6a24c79343075cf8ef85b
tomra <tom.raeves3we@gmail.com>
2021-05-10 12:18:57 +0200

    added printMap

--- src/main/java/com/company/discoveryserver/Status.java
+++ src/main/java/com/company/discoveryserver/Status.java
@@ -1,11 +1,24 @@
 package com.company.discoveryserver;
 
+import java.lang.*;
+import java.util.*;
+
 public class Status {
 
         public static void showStatus() {
                 System.out.println("\nAmount of nodes in the network: " + nodeHandler.nodesMap.size());
                 System.out.println("Map: " + nodeHandler.nodesMap);
-                System.out.println("FileMap: " + fileHandler.filesMap);
+                printMap();
                 System.out.println("\n");
+        }
+
+        public static void printMap(){
+                int counter=0;
+                Iterator<Map.Entry<Integer, File>> iterator = fileHandler.filesMap.entrySet().iterator();
+                while (iterator.hasNext()) {
+                        counter++;
+                        Map.Entry<Integer, File> entry = iterator.next();
+                        System.out.println("File "+counter+" with Name: "+entry.getValue().getFilename()+", ID: "+entry.getKey());
+                }
         }
 }
